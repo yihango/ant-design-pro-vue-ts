12977bf8e93aefcbc7292869171e1bba68428b30
stan e <yi.hang@live.com>
2019-11-03 19:50:24 +0800

    初始化方式修改

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,28 @@
 // ie polyfill
-import '@babel/polyfill'
+import '@babel/polyfill';
 
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store/'
-import { VueAxios } from './utils/request'
-
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/';
+import { VueAxios } from './utils/request';
 // mock
-import './mock'
+import './mock';
 
-import bootstrap from './core/bootstrap'
-import './core/use'
-import './permission' // permission control
-import './utils/filter' // global filter
+import bootstrap from './core/bootstrap';
+import './core/use';
+import './permission'; // permission control
+import './utils/filter'; // global filter
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // mount axios Vue.$http and this.$http
-Vue.use(VueAxios)
+Vue.use(VueAxios);
 
-let initProp = {
+new Vue({
+  // @ts-ignore
   router,
   store,
   created: bootstrap,
   render: h => h(App)
-};
-
-new Vue(initProp).$mount('#app')
+}).$mount('#app');
